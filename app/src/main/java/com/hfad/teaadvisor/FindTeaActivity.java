085e1ddb793ee88cd367f797d37454013bfaf7ce
IanDCarroll <ian.d.carroll@gmail.com>
2016-07-01 11:30:54 -0700

    start building activity

--- app/src/main/java/com/hfad/teaadvisor/FindTeaActivity.java
+++ app/src/main/java/com/hfad/teaadvisor/FindTeaActivity.java
@@ -1,52 +1,24 @@
 package com.hfad.teaadvisor;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.app.Activity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FindTeaActivity extends AppCompatActivity {
+
+public class FindTeaActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_tea);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.content_find_tea);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_find_tea, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    //call when user taps the button
+    public void onClickFindTea(View view) {
+        TextView types = (TextView) findViewById(R.id.types);
+        types.setText("Gup of gea");
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String.valueOf(color.getSelectedItem());
     }
 }
